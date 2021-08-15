ec27c01d39bb990d1255f22949d7d4b6ffb8c8f0
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-15 21:47:20 +0100

    heading only changes after click submit button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [userName, setName] = useState("");
+  const [headinName, setHeading] = useState("");
 
   function handleChange() {
     console.log(event.target.value);
     setName(event.target.value);
   }
+  function handleSubmit() {
+    setHeading(userName);
+  }
 
   return (
     <div className="container">
-      <h1>Hello, {name} </h1>
+      <h1>Hello, {headinName} </h1>
       <input
-        onChange={handleChange}
         type="text"
         placeholder="What's your name?"
+        onChange={handleChange}
+        value={userName}
       />
-      <button>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
